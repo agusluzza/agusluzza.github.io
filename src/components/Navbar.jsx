96999175f7948b3c9e0fcc0a9503540ce0dc0b51
agusluzza <agusluzza@hotmail.com>
2025-04-09 20:54:41 +0000

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,34 +11,10 @@ import {
   Button,
 } from "@heroui/react";
 
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
-
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
+  const menuItems = ["Inicio", "Sobre Mi", "Proyectos", "Contacto"];
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen}>
@@ -47,37 +23,30 @@ export default function App() {
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
-        <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
+          <Link className="hover:text-purple-400" color="foreground" href="#">
+            Inicio
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link className="hover:text-purple-400" aria-current="page" href="#">
+            Sobre Mi
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="hover:text-purple-400" color="foreground" href="#">
+            Proyectos
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
+      <NavbarContent>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <Link className="hover:text-purple-400" color="foreground-" href="#">
+            Contacto
+          </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
@@ -86,7 +55,11 @@ export default function App() {
             <Link
               className="w-full"
               color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
               }
               href="#"
               size="lg"
@@ -99,4 +72,3 @@ export default function App() {
     </Navbar>
   );
 }
-
